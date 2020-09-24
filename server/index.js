b5a1a188a5fa52e1f49b43e1f0f0a81755edfad9
Sam Smith <sam@causalens.com>
2020-09-24 09:29:09 +0100

    Updated the test to remove file uploading part so the candidate can focus more on the dashboard

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const csv = require("csv-parser");
 const express = require("express");
+const fs = require("fs");
 const difference = require("lodash/difference");
+
+const data = [];
+fs.createReadStream("demo_data.csv")
+    .pipe(csv())
+    .on("data", (row) => {
+        data.push(row);
+    })
+    .on("end", () => {
+        console.log("Data Loaded Successfully");
+    });
 
 const app = express();
 app.use(cors());
@@ -9,72 +21,39 @@ app.use(bodyParser.json());
 const port = 3001;
 
 /**
- * Main endpoint for predicting the some results from the data and target provided. The data should be in json form and
- * can contain any number of fields. Those fields must be consistent between all the objects, but can be null, and every
- * record must contain an index key. The value of index can either be an increasing integer or a timestamp and both
- * should be supported in the UI.
- *
- * Request body structure should be as follows:
- *
- * {
- *     data: [{
- *          index: 0,
- *          price: 123,
- *          interestRate: 0.3,
- *          borrowRate: 0.5,
- *          marketCap: 1e14,
- *     }, {
- *          index: 1,
- *          price: 456,
- *          interestRate: 0.32,
- *          borrowRate: 0.4,
- *          marketCap: 1e13,
- *     }]
- *     target: 'price'
- * }
+ * List the targets available from the dataset that is currently loaded
  */
-app.post("/predict", (req, res) => {
+app.get("/list-targets", (req, res) => {
+    const targets = Object.keys(data[0]).filter((col) => col !== "index");
+    res.send(targets);
+});
+
+/**
+ * Main endpoint for predicting the results from the target provided. The target should be provided as a string as
+ * returned by the list-targets endpoint
+ */
+app.get("/predict", (req, res) => {
+    const target = req.query.target;
+
     // Run some validation on the input
-    if (!req.body.data || !req.body.target || !Array.isArray(req.body.data)) {
+    if (!target) {
         throw new Error(
-            "Invalid request passed to endpoint, should be an object with data and target keys"
+            "Invalid request passed to endpoint, missing target from query params"
         );
     }
 
-    const validKeys = Object.keys(req.body.data[0]);
-    const indexes = [];
-
-    for (let item of req.body.data) {
-        if (difference(Object.keys(item), validKeys).length !== 0) {
-            throw new Error(
-                "Some of the objects passed have different keys, each item passed should have a consistent structure"
-            );
-        }
-        if (item.index === undefined || item.index === null) {
-            throw new Error("Every item must have an index field");
-        }
-        if (indexes.includes(item.index)) {
-            throw new Error("The index value of each item must be unique");
-        }
-        indexes.push(item.index);
-        if (
-            item[req.body.target] === undefined ||
-            item[req.body.target] === null
-        ) {
-            throw new Error("Target must be defined for every timestep");
-        }
-    }
+    const validKeys = Object.keys(data[0]);
 
     // Build out the fake responses for predictions and featureImportances
     const predictions = [];
-    for (let item of req.body.data) {
-        predictions.push(item[req.body.target] * (Math.random() + 0.5));
+    for (let item of data) {
+        predictions.push(item[target] * (Math.random() + 0.5));
     }
 
     const featureImportance = {};
     let remaining = 1;
     for (let key of validKeys) {
-        if (key === req.body.target || key === "index") {
+        if (key === target || key === "index") {
             continue;
         }
         const importance = Math.random();
@@ -88,7 +67,7 @@ app.post("/predict", (req, res) => {
     }
 
     const confusionMetric = {};
-    let confusionRemaining = req.body.data.length;
+    let confusionRemaining = data.length;
     for (let key of ["falsePositive", "truePositive", "falseNegative"]) {
         const count = Math.floor(Math.random() * confusionRemaining);
         confusionMetric[key] = count;
